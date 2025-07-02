19c789f1e707ad1d715d0a268754ecfe91917821
Efrenoscar Arnaude <efrenoscar6@gmal.com>
2025-07-02 18:29:58 +0200

    Add allowed host kachaapp.kachafruitbcn.com

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,7 @@ SECRET_KEY = env(
     default="ZhNQ5aOV1JZjigrMtVB48aIwNfUoZ8x7nT2evdG3MgfnYNauGKEOsKIDuzspsRis",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "kachaapp.kachadigitalbcn.com"]  # noqa: S104
 
 # CACHES
 # ------------------------------------------------------------------------------
